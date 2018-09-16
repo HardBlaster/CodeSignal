018aa98d86175cb2635f5746781a467227108c0f
HardBlaster <33747335+HardBlaster@users.noreply.github.com>
2018-09-16 12:19:59 +0200

    Delete Make_Array_Consecutive_2.java

--- Make_Array_Consecutive_2.java
+++
@@ -1,9 +0,0 @@
-int makeArrayConsecutive2(int[] statues) {
-    Arrays.sort(statues);
-    int addon = 0;
-    for(int i = 0; i < statues.length-1; i++)
-    {
-        addon += statues[i+1]-statues[i]-1;
-    }
-    return addon;
-}
