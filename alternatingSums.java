fd520a2a4b697b21bae838a8ddcb6cc5116235aa
HardBlaster <33747335+HardBlaster@users.noreply.github.com>
2018-09-16 12:20:25 +0200

    Delete alternatingSums.java

--- alternatingSums.java
+++
@@ -1,7 +0,0 @@
-int[] alternatingSums(int[] a) {
-    int len = a.length;
-    int[] sums = new int[2];
-    for(int i = 0; i < len; i += 2) sums[0] += a[i];
-    for(int i = 1; i < len; i += 2) sums[1] += a[i];
-    return sums;
-}
