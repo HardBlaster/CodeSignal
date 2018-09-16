7bccad3a4806bbb70e6e61eb5b2c3ec1877daaec
HardBlaster <33747335+HardBlaster@users.noreply.github.com>
2018-09-16 12:20:41 +0200

    Delete arrayMaximalAdjacentDifference.java

--- arrayMaximalAdjacentDifference.java
+++
@@ -1,7 +0,0 @@
-int arrayMaximalAdjacentDifference(int[] inputArray) {
-    int len = inputArray.length;
-    int max = 0;
-    for(int i = 0; i < len-1; i++)
-        if(Math.abs(inputArray[i] - inputArray[i+1]) > max) max = Math.abs(inputArray[i] - inputArray[i+1]);
-    return max;
-}
