aafb2e9df5f7494ae0ec49d279ac00b63f3f86a5
HardBlaster <33747335+HardBlaster@users.noreply.github.com>
2018-09-16 12:20:31 +0200

    Delete areEquallyStrong.java

--- areEquallyStrong.java
+++
@@ -1,4 +0,0 @@
-boolean areEquallyStrong(int yourLeft, int yourRight, int friendsLeft, int friendsRight) {
-    if((yourLeft == friendsLeft & yourRight == friendsRight) || (yourLeft == friendsRight & yourRight == friendsLeft)) return true;
-    else return false;
-}
