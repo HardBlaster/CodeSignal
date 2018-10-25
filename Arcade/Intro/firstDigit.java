99c19acfba99e5303d6dab2ee3c563da7d4a87fe
HardBlaster <33747335+HardBlaster@users.noreply.github.com>
2018-10-25 18:32:51 +0200

    Create firstDigit.java

---
+++ Arcade/Intro/firstDigit.java
@@ -0,0 +1,5 @@
+char firstDigit(String inputString) {
+    for(int i = 0; i < inputString.length(); i++)
+        if(Character.isDigit(inputString.charAt(i))) return inputString.charAt(i);
+    return 0;
+}
